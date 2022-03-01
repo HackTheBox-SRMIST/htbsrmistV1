a83553a37c40909ee6d20f9be236b80564103cd7
Sudhanshu Srivastava <71897832+Codered9@users.noreply.github.com>
2022-03-01 21:45:20 +0530

    Index Page with Discord link and link to index page

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+const Nav = () => {
+    return (
+        <div>
+            <a href="/" rel="noopener noreferrer">
+                <img src="/logo.png" alt="" className="w-20 -mb-12 md:w-28 md:-mb-24" />
+            </a>
+        </div>
+    )
+}
+
+export default Nav;
