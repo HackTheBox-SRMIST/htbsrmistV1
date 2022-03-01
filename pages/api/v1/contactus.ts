482d324d915d87c20c5b2f5e570b1a030c54e437
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2022-03-01 18:23:58 +0530

    ✨ feat: add init api and boilerplate code

---
+++ pages/api/v1/contactus.ts
@@ -0,0 +1,8 @@
+import { NextApiHandler } from "next";
+const contactUs: NextApiHandler = (req, res) => {
+  res.json({
+    message: `You have requested contact us`,
+  });
+};
+
+export default contactUs;
---
+++ pages/api/v1/team.ts
@@ -0,0 +1,8 @@
+import { NextApiHandler } from "next";
+const getTeam: NextApiHandler = (req, res) => {
+  res.json({
+    message: `You have requested team`,
+  });
+};
+
+export default getTeam;
