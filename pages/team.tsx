9edc2b6af1560735dd798592e23019c640f09319
Sudhanshu Srivastava <71897832+Codered9@users.noreply.github.com>
2022-04-30 16:23:20 +0000

    🐛 Fixed Bug 2 and 4.

--- pages/team.tsx
+++ pages/team.tsx
@@ -8,7 +8,7 @@ import TwitterLogo from "../utils/icons/TwitterLogo";
 import { ReactChild, ReactFragment, ReactPortal } from "react";
 const Team: NextPage = ({ data }: any) => {
     return (
-        <div className="">
+        <div className="w-full">
             <Head>
                 <title>Team | HackTheBox SRMIST</title>
                 <meta name="title" content="HackTheBox SRMIST" />
@@ -56,7 +56,7 @@ const Team: NextPage = ({ data }: any) => {
             <p className="text-4xl mx-10 lg:mx-20 md:text-6xl text-htb-green text-bold md:ml-16">
                 <strong>OUR TEAM</strong>
             </p>
-            <div className="grid md:grid-cols-3 gap-8 md:gap-20 my-6 mx-8 md:mx-28 lg:mx-48">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3  my-6 justify-center">
                 {/* {members.map(member => ( */}
                 {data.map(
                     (member: {
@@ -76,51 +76,52 @@ const Team: NextPage = ({ data }: any) => {
                             | null
                             | undefined;
                         socials: {
-                            linkdin: string | undefined;
+                            linkedin: string | undefined;
                             github: string | undefined;
                             twitter: string | undefined;
                         };
                     }) => (
-                        <div>
-                            <figure className="md:flex flex-col rounded-xl lg:p-8 md:p-0 hover:bg-hacker-grey hover:bg-opacity-70 dark:bg-slate-800 ">
+                        <div className="">
+                            <figure className="md:flex flex-col rounded-xl md:p-0 hover:bg-hacker-grey hover:bg-opacity-70 dark:bg-slate-800 py-8 w-full md:max-w-4/5 items-center min-h-[28em] relative mr-0">
                                 <img
-                                    className="w-24 h-24 md:w-48 md:h-auto rounded-full mx-auto mt-4"
+                                    className="w-24 h-24 md:w-48 md:h-[12em] rounded-full mx-auto mt-4"
                                     src={member.pictureUrl}
                                     alt=""
                                     width="384"
                                     height="512"
+                                    style={{objectFit: "cover"}}
                                 />
-                                <div className="pt-6 lg:p-8 text-center md:text-left space-y-4">
+                                <div className="pt-6 text-center space-y-4 px-8">
                                     <figcaption className="font-medium text-center ">
                                         <div className="text-white text-xl font-bold hover:text-htb-green">
                                             <strong>{member.name}</strong>
                                         </div>
                                     </figcaption>
                                     <blockquote>
-                                        <p className="text-lg font-medium text-white text-center lg:px-[2em]">
+                                        <p className="text-lg font-medium text-white text-center  break-all">
                                             {/* {member.description} */}
                                             {member.caption}
                                         </p>
                                     </blockquote>
-                                    <div className="flex justify-center">
+                                    <div className="flex justify-center space-x-3 absolute bottom-8 left-[7.5rem] ">
                                         <a
-                                            href={member.socials.linkdin}
+                                            href={member.socials.linkedin}
                                             target="_blank"
-                                            className="rounded-full opacity-60 hover:opacity-100 mx-2"
+                                            className="rounded-full opacity-60 hover:opacity-100"
                                         >
                                             <LinkedInLogo />
                                         </a>
                                         <a
                                             href={member.socials.github}
                                             target="_blank"
-                                            className="rounded-full opacity-60 hover:opacity-100 mx-2"
+                                            className="rounded-full opacity-60 hover:opacity-100"
                                         >
                                             <GithubLogo />
                                         </a>
                                         <a
                                             href={member.socials.twitter}
                                             target="_blank"
-                                            className="rounded-full opacity-60 hover:opacity-100 mx-2"
+                                            className="rounded-full opacity-60 hover:opacity-100"
                                         >
                                             <TwitterLogo />
                                         </a>
@@ -157,6 +158,7 @@ export async function getServerSideProps() {
     const res = await fetch(`${url_root}/api/v1/teams/?current=true`);
     const body = await res.json();
     const data = body.data;
+    console.log(data);
     return { props: { data } };
 }
 
