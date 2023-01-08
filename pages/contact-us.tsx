8f63655177916fc6e9afaa5bf7f8ce0197519789
Mehul2711 <mehul271102@gmail.com>
2023-01-08 17:43:56 +0530

    update contact and footer socials

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -3,8 +3,12 @@ import Nav from "../components/navbar";
 import Footer from "../components/footer";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import PhoneInputWithCountrySelect from "react-phone-number-input";
+import { useState } from "react";
 const base_url = process.env.BASE_URL_PREVIEW;
 const ContactUs = () => {
+    const [value, setValue] = useState("+91");
+
     const submitHandler = async (event: React.ChangeEvent<any>) => {
         event.preventDefault();
         const body = JSON.stringify({
@@ -57,6 +61,7 @@ const ContactUs = () => {
         }
         console.log(body);
     };
+
     return (
         <div>
             <Head>
@@ -65,29 +70,41 @@ const ContactUs = () => {
             </Head>
 
             <ToastContainer />
-            <section className="md:w-6/12 lg:w-7/12 text-center mx-auto flex flex-col space-y-6 p-4 md:p-0 md:mb-12">
-                <h1 className="md:text-7xl ml-1.5 text-5xl my-2 text-white font-bold">
-                    Contact Us
-                </h1>
-                <p className="text-white text-lg">
+            <section className="md:w-6/12 text-center mx-auto flex flex-col space-y-6 p-4 md:p-12 border-htb-green/50 border-2 border-solid rounded-3xl md:mb-12 backdrop-blur-3xl ">
+                <div className="mb-5">
+                    <span className="md:text-7xl text-5xl text-white font-bold border-b-2 border-solid border-htb-green/50">
+                        Contact Us
+                    </span>
+                </div>
+
+                {/* <p className="text-htb-green/50 text-lg">
                     Hey, wanna have some chit chat with us. Feel free to reach
                     out. New ideas, event collaborations, feedbacks, want to
                     offer Bagel (so sweet of you) anything. We up for it. ;)
                     (wink wink).
-                </p>
+                </p> */}
                 <form
                     onSubmit={submitHandler}
-                    className="flex-col gap-y-8 flex"
+                    className="flex-col gap-y-5 flex"
                 >
-                    <div className="">
+                    <div className="flex justify-between">
                         <input
                             id="name"
                             name="name"
                             type="text"
                             autoComplete="name"
                             required
-                            placeholder="Name"
-                            className="w-full p-4 bg-[#C4C4C4] required:border-red-500 placeholder-gray-800"
+                            placeholder="First Name"
+                            className="w-[48%] p-3 bg-[#1e2a3e] required:border-red-500 placeholder-htb-green/50 text-htb-green focus:outline-none"
+                        />
+                        <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            autoComplete="name"
+                            required
+                            placeholder="Last Name"
+                            className="w-[48%] p-4 bg-[#1e2a3e] required:border-red-500 placeholder-htb-green/50 text-htb-green focus:outline-none"
                         />
                     </div>
                     <div className="">
@@ -98,7 +115,7 @@ const ContactUs = () => {
                             autoComplete="email"
                             placeholder="Email"
                             required
-                            className="w-full p-4 bg-[#C4C4C4] placeholder-gray-800"
+                            className="w-full p-4 bg-[#1e2a3e] required:border-red-500 placeholder-htb-green/50 text-htb-green focus:outline-none"
                         />
                     </div>
                     <div className="flex flex-row">
@@ -107,17 +124,24 @@ const ContactUs = () => {
                             name="countryCode"
                             autoComplete="countryCode"
                             placeholder="+91"
-                            className="flex-shrink md:w-16 w-12 pl-2 placeholder-gray-800"
+                            className="flex-shrink md:w-16 w-12 pl-2 bg-[#2b3a56] required:border-red-500 placeholder-htb-green/50 text-htb-green focus:outline-none"
                         />
                         <input
                             id="number"
                             name="number"
-                            type="text"
+                            type="tel"
                             autoComplete="phone"
                             placeholder="Contact Number (Not Required)"
-                            className="w-full p-4 bg-[#C4C4C4] placeholder-gray-800"
+                            className="w-full p-4 bg-[#1e2a3e] required:border-red-500 placeholder-htb-green/50 text-htb-green focus:outline-none"
                         />
                     </div>
+                    {/* <div>
+                        <PhoneInputWithCountrySelect
+                            onChange={setValue}
+                            value={value}
+                            placeholder="Enter phone number"
+                        />
+                    </div> */}
                     <div className="">
                         <textarea
                             id="message"
@@ -125,13 +149,13 @@ const ContactUs = () => {
                             autoComplete="message"
                             required
                             placeholder="Message"
-                            className="w-full p-4 bg-[#C4C4C4] message placeholder-gray-800"
+                            className="w-full p-4 bg-[#1e2a3e] required:border-red-500 placeholder-htb-green/50 text-htb-green focus:outline-none"
                             rows={10}
                         ></textarea>
                     </div>
                     <button
                         type="submit"
-                        className="w-full bg-htb-green py-2 font-normal text-lg"
+                        className="w-full bg-htb-green/50 hover:bg-htb-green py-2 font-normal text-lg"
                     >
                         SEND MESSAGE
                     </button>
