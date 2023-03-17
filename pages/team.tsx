56765380a0357c3fcdadaf3c1031b23c6608638e
Aniruddha Ghosh <aghosh0605@gmail.com>
2023-03-18 00:56:44 +0530

    📝 Added Memebers Role

--- pages/team.tsx
+++ pages/team.tsx
@@ -34,8 +34,9 @@ const hierarchy = [
     { role: "Faculty Convenor", name: "Mainframe" },
     { role: "Co-Organizers", name: "Kernel" },
     { role: "Admins", name: "Root" },
-    { role: "Sudoers", name: "Leads" },
-    { role: "Sticky Bits", name: "Associates" }
+    { role: "Leads", name: "Sudoer" },
+    { role: "Associates", name: "Sticky Bit" },
+    { role: "Members", name: "Binary" }
 ];
 
 const Team: NextPage<TeamPageProps> = ({ members }) => {
