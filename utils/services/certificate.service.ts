4e9e6ba252f4945838bc1a754132116f7034fa93
sarthakk24 <sarthak.sachdeva.73@gmail.com>
2023-04-09 19:37:45 +0530

    :bug: fix jimp

--- utils/services/certificate.service.ts
+++ utils/services/certificate.service.ts
@@ -24,7 +24,7 @@ const textOverlay = async (
         //     jimp_options[`FONT_${font_size}_${color.toUpperCase()}`];
 
         const jimp_font = path.resolve(
-            `fonts/open-sans/open-sans-${font_size}-${color.toLowerCase()}/open-sans-${font_size}-${color.toLowerCase()}.fnt`
+            `public/fonts/open-sans/open-sans-${font_size}-${color.toLowerCase()}/open-sans-${font_size}-${color.toLowerCase()}.fnt`
         );
 
         const image = await Jimp.read(`${url}`);
