193d4016a70fb0a351254224f287a6aa66b54b16
WraitH17 <saranshsinha084@gmail.com>
2024-02-03 02:44:23 +0530

    :sparkles: Add registration mailer

--- pages/api/v1/events/registration.ts
+++ pages/api/v1/events/registration.ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import errorHandler from "../../../../utils/error/errorHandler";
 import { DBInstance } from "../../../../utils/db.connect";
+import { sendConfirmationEmail } from "../../../../utils/awsServices/sesMailer";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
-        if (req.method == "POST") {
-            console.log(req.body);
+        if (req.method === "POST") {
             const { usn, name, email, dept, isSrmite, event_name } = req.body;
             if (!usn || !name || !email || !dept || !isSrmite || !event_name) {
                 return res.status(406).json({
@@ -14,8 +14,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                     data: null
                 });
             }
-            const dbInstance = await DBInstance.getInstance();
 
+            const dbInstance = await DBInstance.getInstance();
             const eventsCollection = await dbInstance.getCollection("events");
             const eventData: any = await eventsCollection.findOne({
                 event_name: event_name
@@ -24,57 +24,66 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                 "participants",
                 eventData.database
             );
-            const participants = await participantsCollection.findOne({
+            const existingParticipant = await participantsCollection.findOne({
                 $or: [{ usn: usn }, { email: email }]
             });
-            if (participants) {
+
+            if (existingParticipant) {
                 return res.status(406).json({
                     success: false,
                     message: "❌ Already registered for the event",
                     data: null
                 });
             }
+
             const extraData = {
-                checkin: {
-                    status: false, // false
-                    modified_at: Date.now() // null
-                },
-                checkout: {
-                    status: false,
-                    modified_at: Date.now()
-                },
-                snacks: {
-                    status: false,
-                    modified_at: Date.now()
-                },
+                checkin: { status: false, modified_at: Date.now() },
+                checkout: { status: false, modified_at: Date.now() },
+                snacks: { status: false, modified_at: Date.now() },
                 certificate_status: {
                     email_uid: null,
                     status: false,
                     modified_at: Date.now()
                 },
-                is_onsite: {
-                    status: false,
-                    modified_at: Date.now()
-                },
-                is_rsvp: {
-                    status: false,
-                    modified_at: Date.now()
-                }
+                is_onsite: { status: false, modified_at: Date.now() },
+                is_rsvp: { status: false, modified_at: Date.now() }
             };
 
-            const data = await participantsCollection.insertOne({
+            const insertedParticipant = {
                 usn,
                 name,
                 email,
                 dept,
                 isSrmite,
                 ...extraData
-            });
-            res.status(200).json({
-                success: true,
-                message: `✅ Successfully Registered user ${name}`,
-                data: data
-            });
+            } as any;
+
+            const data = await participantsCollection.insertOne(
+                insertedParticipant
+            );
+
+            // Send confirmation email and check for success
+            try {
+                await sendConfirmationEmail(
+                    insertedParticipant,
+                    event_name,
+                    eventData
+                );
+                res.status(200).json({
+                    success: true,
+                    message: `✅ Successfully Registered user ${name} and confirmation email sent`,
+                    data: data
+                });
+            } catch (emailError: any) {
+                // If email sending fails, you may want to handle it gracefully
+                console.error("Error sending confirmation email:", emailError);
+                res.status(500).json({
+                    success: false,
+                    message:
+                        "❌ Registration successful, but failed to send confirmation email",
+                    data: data
+                });
+            }
         } else {
             console.log("🚫", req.method, "was called and got error!!");
             res.status(405).json({
