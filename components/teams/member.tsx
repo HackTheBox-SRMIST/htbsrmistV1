87b8b0506550f2130b106cd809c31dce6b67984e
sabarish <vbsabarishsriram@gmail.com>
2024-03-24 00:31:09 +0530

    team-hover-border

--- components/teams/member.tsx
+++ components/teams/member.tsx
@@ -11,11 +11,11 @@ const socialIcons = {
 
 const Member = (props: any) => {
     return (
-        <div className="text-white flex flex-col items-center py-10 px-2 w-auto  ">
+        <div className="text-white flex flex-col items-center py-10 mt-2 transition duration-300 px-2 w-auto rounded-3xl hover:shadow-[0_0_2px_#8bef00,inset_0_0_2px_#8bef00,0_0_5px_#8bef00,0_0_15px_#8bef00,0_0_#8bef00]">
             <img
                 // src="https://i.ndtvimg.com/i/2017-03/rowan-atkinson_640x480_71490079191.jpg"
                 src={props.image}
-                className="rounded-full border-2 border-solid border-htb-green/50  p-1 w-40 h-40 hover:w-64 hover:h-64 ease-in-out duration-300 bg-cover bg-center object-cover brightness-125"
+                className="rounded-full border-2 border-solid border-htb-green/50  p-1 w-40 h-40 bg-cover bg-center object-cover brightness-125"
             />
 
             <span className="text-3xl pt-5 text-htb-green font-semibold">
